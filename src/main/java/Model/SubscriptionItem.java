c8bf84b352487e489cc92110f95bd6f9dfeaefba
tikakurnsri <147170238+tikakurnsri@users.noreply.github.com>
2024-06-21 14:12:25 +0800

    Update SubscriptionItem.java

--- src/main/java/Model/SubscriptionItem.java
+++ src/main/java/Model/SubscriptionItem.java
@@ -1,27 +1,27 @@
 package Model;
 
 public class SubscriptionItem {
-    private int itemId;
+    private int item;
     private int quantity;
     private double price;
     private double amount;
-    private Item item;
+    private Item items;
 
     public SubscriptionItem() {}
 
     public SubscriptionItem(int itemId, int quantity, double price, double amount) {
-        this.itemId = itemId;
+        this.item = itemId;
         this.quantity = quantity;
         this.price = price;
         this.amount = amount;
     }
 
-    public int getItemId() {
+    public int getItem() {
         return itemId;
     }
 
     public void setItemId(int itemId) {
-        this.itemId = itemId;
+        this.item = item;
     }
 
     public int getQuantity() {
@@ -59,7 +59,7 @@ public class SubscriptionItem {
     @Override
     public String toString() {
         return "SubscriptionItem{" +
-                "itemId=" + itemId +
+                "item=" + itemId +
                 ", quantity=" + quantity +
                 ", price=" + price +
                 ", amount=" + amount +
